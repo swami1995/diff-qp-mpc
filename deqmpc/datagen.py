added new sac policy and fixed pendulum normalization stuff for deq and diffmpc

deqmpc/datagen.py
@@ -137,9 +137,10 @@ def get_pendulum_expert_traj_ppo(env, num_traj):
 def get_pendulum_expert_traj_sac(env, num_traj):
     """
     Get expert trajectories for pendulum environment using the saved PPO checkpoint."""
-    device = torch.device("cuda" if True else "cpu")
+    device = torch.device("cuda" if False else "cpu")
     policy = GaussianPolicy(env.observation_space.shape[0], env.action_space.shape[0], 256, env.action_space).to(device)
-    checkpoint = torch.load("/home/sgurumur/locuslab/pytorch-soft-actor-critic/checkpoints/sac_checkpoint_Pendulum-v0_bestT200")
+    # checkpoint = torch.load("/home/sgurumur/locuslab/pytorch-soft-actor-critic/checkpoints/sac_checkpoint_Pendulum-v0_bestT200")
+    checkpoint = torch.load("ckpts/sac/sac_checkpoint_Pendulum-v0_bestT200")
     policy.load_state_dict(checkpoint['policy_state_dict'])
     trajectories = []
     reward_trajs = []
@@ -186,7 +187,7 @@ def save_expert_traj(env, num_traj, type='mpc'):
     if os.path.exists('data') == False:
         os.makedirs('data')
         
-    with open(f'data/expert_traj_{type}-{env.spec_id}.pkl', 'wb') as f:
+    with open(f'data/expert_traj_{type}-{env.spec_id}_new.pkl', 'wb') as f:
         pickle.dump(expert_traj, f)
 
 def get_gt_data(args, env, type='mpc'):
@@ -199,7 +200,8 @@ def get_gt_data(args, env, type='mpc'):
     Returns:
         A list of trajectories, each trajectory is a list of (state, action) tuples.
     """
-    with open('data/expert_traj_mpc-Pendulum-v0.pkl', 'rb') as f:#f'data/expert_traj_{type}-{env.spec_id}.pkl', 'rb') as f:
+    # with open('data/expert_traj_mpc-Pendulum-v0.pkl', 'rb') as f:#f'data/expert_traj_{type}-{env.spec_id}.pkl', 'rb') as f:
+    with open(f'data/expert_traj_{type}-{env.spec_id}_new.pkl', 'rb') as f:
         gt_trajs = pickle.load(f)
     # ipdb.set_trace()
     return gt_trajs
@@ -280,5 +282,5 @@ if __name__ == '__main__':
     print("Starting!")
     # ipdb.set_trace()
     env = PendulumEnv(stabilization=False)
-    # save_expert_traj(env, 1, 'mpc')
-    test_qp_mpc(env)
+    save_expert_traj(env, 200, 'sac')
+    # test_qp_mpc(env)

deqmpc/envs.py
@@ -43,6 +43,13 @@ class PendulumDynamics(torch.nn.Module):
 
         return newthdot, newthdotdot
     
+    def action_clip(self, action):
+        return torch.clamp(action, -self.max_torque, self.max_torque)
+    
+    def state_clip(self, state):
+        state[..., 0] = angle_normalize(state[..., 0])
+        return state
+    
 def angle_normalize(x):
     return (((x+np.pi) % (2*np.pi)) - np.pi)
 
@@ -89,7 +96,8 @@ class PendulumEnv:
             high = np.array([0.05, 0.5])
         else:
             high = np.array([np.pi, 1])
-        self.state = torch.tensor(np.array([np.pi, 1]), dtype=torch.float32)#np.random.uniform(low=-high, high=high), dtype=torch.float32)
+        self.state = torch.tensor(np.array([np.pi, 1]), dtype=torch.float32)#
+        self.state = torch.tensor(np.random.uniform(low=-high, high=high), dtype=torch.float32)
         self.num_successes = 0
         return self.state.numpy()
 
@@ -103,7 +111,9 @@ class PendulumEnv:
         """
         # action = torch.tensor([action], dtype=torch.float32)
         action = torch.tensor(action, dtype=torch.float32)
+        action = self.dynamics.action_clip(action)
         self.state = self.dynamics(self.state, action)
+        self.state = self.dynamics.state_clip(self.state)
         done = self.is_done()
         reward = self.get_reward(action)  # Define your reward function based on the state and action
         return self.state.numpy(), reward, done, {}
@@ -118,7 +128,8 @@ class PendulumEnv:
         # For demonstration, let's say an episode ends if the pendulum is upright within a small threshold
         # ipdb.set_trace()
         # theta, _ = self.state.unbind()
-        theta, _ = self.state[0][0], self.state[0][1]
+        # theta, _ = self.state[0][0], self.state[0][1]
+        theta, _ = self.state[0], self.state[1]
         success = abs(angle_normalize(theta)) < 0.05
         self.num_successes = 0 if not success else self.num_successes + 1
         return self.num_successes >= 10
@@ -134,7 +145,8 @@ class PendulumEnv:
         # Define your reward function; for simplicity, let's use the negative square of the angle
         # as a reward, so the closer to upright (0 rad), the higher the reward.
         # theta, _ = self.state.unbind()
-        theta, _ = self.state[0][0], self.state[0][1]
+        # theta, _ = self.state[0][0], self.state[0][1]
+        theta, _ = self.state[0], self.state[1]
         return -float(angle_normalize(theta) ** 2)
 
     def close(self):

deqmpc/train_simple.py
@@ -33,7 +33,7 @@ def main():
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     env = PendulumEnv(stabilization=False)
-    gt_trajs = get_gt_data(args, env, "mpc")
+    gt_trajs = get_gt_data(args, env, "sac")
     gt_trajs = merge_gt_data(gt_trajs)
     args.Q = torch.Tensor([10.0, 1]).to(args.device)
     args.R = torch.Tensor([1.0]).to(args.device)
@@ -66,6 +66,7 @@ def main():
         # x_gt = gt_trajs[idxs, t_idxs:t_idxs+args.T]
 
         # ipdb.set_trace()
+        traj_sample["state"] = unnormalize_states(traj_sample["state"])
         if args.deq:
             loss = 0.0
             trajs = policy(traj_sample["state"][:, 0])
@@ -102,11 +103,24 @@ def main():
                 "grad norm: ",
                 torch.nn.utils.clip_grad_norm_(policy.model.parameters(), 1000),
             )
-            print("loss: ", np.mean(losses)/6, "loss_end: ", np.mean(losses_end))
+            print("loss: ", np.mean(losses)/args.deq_iter, "loss_end: ", np.mean(losses_end))
             losses = []
             # print('nominal states: ', nominal_states)
             # print('nominal actions: ', nominal_actions)
 
+def unnormalize_states(nominal_states):
+    # ipdb.set_trace()
+    # check theta of the first state in nominal_states[:, 0][0] and make sure all the nominal_states are in the same phase (i.e in terms of angle normalization)
+    angle_0 = nominal_states[:, 0, 0]
+    prev_angle = angle_0
+    # ipdb.set_trace()
+    for i in range(nominal_states.shape[1]):
+        mask = torch.abs(nominal_states[:, i, 0] - prev_angle) > np.pi/2
+        mask_sign = torch.sign(nominal_states[:, i, 0])
+        if mask.any():
+            nominal_states[mask, i, 0] = nominal_states[mask, i, 0] - mask_sign[mask]*2*np.pi 
+        prev_angle = nominal_states[:, i, 0]
+    return nominal_states
 
 if __name__ == "__main__":
     main()