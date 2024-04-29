fixed AL gradient issues

deqmpc/envs.py
@@ -98,7 +98,8 @@ class PendulumEnv:
         self.action_space = Spaces(-np.array([self.max_torque]), np.array([self.max_torque]), (self.nu, 2)) #np.array([[-2, 2]])                
         self.stabilization = stabilization
         self.Qlqr = torch.Tensor([10.0, 1.00])
-        self.Rlqr = torch.Tensor([0.0000000])
+        # self.Rlqr = torch.Tensor([0.0000000])
+        self.Rlqr = torch.Tensor([0.001])
 
     def seed(self, seed):
         """
@@ -264,7 +265,7 @@ class IntegratorEnv:
         self.action_space = Spaces(-np.full(self.nu, self.max_acc), np.full(self.nu, self.max_acc), (self.nu, 2))
 
         self.Qlqr = torch.Tensor([10.0, 1.00])
-        self.Rlqr = torch.Tensor([0.00001])
+        self.Rlqr = torch.Tensor([0.001])
 
     def seed(self, seed):
         """

deqmpc/policies.py
@@ -388,9 +388,10 @@ class DEQMPCPolicy(torch.nn.Module):
             # torch.cuda.synchronize()
             # start = time.time()
             # x_ref = (x_ref.view(bsz, self.T, -1)*mask[:, :, None]).view(bsz, -1)
-            if iter == 1000 or iter == 1100 or iter == 1200 or iter== 1300:
-                self.tracking_mpc.ctrl.verbose = 1
+            # if iter == 1000 or iter == 1100 or iter == 1200 or iter== 1300:
+            #     self.tracking_mpc.ctrl.verbose = 1
             #     ipdb.set_trace()
+            # ipdb.set_trace()
             x_ref, z = self.model(x_ref, z)
             x_ref = x_ref.view(-1, self.T-1, self.np)
             x_ref = torch.cat([x[:, None, :], x_ref], dim=1)
@@ -409,6 +410,7 @@ class DEQMPCPolicy(torch.nn.Module):
             # u_gt = u_gt[0][None].repeat(bsz, 1, 1)
             # x = x[0][None].repeat(bsz, 1)
             # x_ref = x_gt#
+            # u_ref = torch.randn_like(x_ref[..., :self.nu])*1.0#
             u_ref = torch.zeros_like(x_ref[..., :self.nu])
             xu_ref = torch.cat(
                 [x_ref, u_ref], dim=-1
@@ -427,7 +429,7 @@ class DEQMPCPolicy(torch.nn.Module):
                 # ipdb.set_trace()
                 # self.tracking_mpc.ctrl.al_iter = 2*(i+1)
                 nominal_states, nominal_actions = self.tracking_mpc(x, xu_ref, x_ref_tr, u_ref_tr)
-                
+                # ipdb.set_trace()
                 # torch.cuda.synchronize()
                 # end = time.time()
                 # self.mpc_time.append(end-start)
@@ -436,7 +438,7 @@ class DEQMPCPolicy(torch.nn.Module):
                 # ipdb.set_trace()
             nominal_states_net = x_ref#.transpose(0, 1)
             if lastqp_solve:
-                trajs.append((nominal_states_net, nominal_states.detach().clone(), nominal_actions.detach().clone()))
+                trajs.append((nominal_states_net.detach().clone(), nominal_states.detach().clone(), nominal_actions.detach().clone()))
             else:
                 trajs.append((nominal_states_net, nominal_states, nominal_actions))
             # x_ref = nominal_states_net.transpose(0, 1).reshape(bsz, -1)#.detach().clone().reshape(bsz, -1)
@@ -445,7 +447,8 @@ class DEQMPCPolicy(torch.nn.Module):
             else:
                 x_ref = nominal_states.reshape(bsz, -1).detach().clone()
             # x_ref = x_ref.reshape(bsz, -1).detach().clone()
-        self.tracking_mpc.ctrl.verbose = 0
+        # ipdb.set_trace()
+        # self.tracking_mpc.ctrl.verbose = 0
         # print(f"Network time: {np.mean(self.network_time)} MPC time: {np.mean(self.mpc_time)}")
         dyn_res = (self.tracking_mpc.dyn(x_ref.view(-1, self.nx).double(), nominal_actions.view(-1, self.nu).double()).view(bsz, self.T, -1)[:, :-1] - x_ref.view(bsz, self.T, -1)[:, 1:]).view(bsz, -1).norm(dim=1).mean().item()
         # dyn_res = (self.tracking_mpc.dyn(x_gt.view(-1, self.nx).double(), u_gt.view(-1, self.nu).double()).view(bsz, 3, -1)[:, :-1] - x_gt[:, 1:]).view(bsz, -1).norm(dim=1).mean().item()
@@ -454,7 +457,6 @@ class DEQMPCPolicy(torch.nn.Module):
         if lastqp_solve:
             # for i in range(5):
             #     nominal_states, nominal_actions = self.tracking_mpc(x, xu_ref, x_ref_tr, u_ref_tr)
-            
             nominal_states, nominal_actions = self.tracking_mpc(x, xu_ref, x_ref_tr, u_ref_tr)
             trajs[-1] = (nominal_states_net, nominal_states, nominal_actions)
         return trajs, dyn_res
@@ -540,7 +542,7 @@ class Tracking_MPC(torch.nn.Module):
                 u_upper=self.u_upper,
                 # al_iter=self.qp_iter,
                 exit_unconverged=False,
-                eps=1e-5,
+                eps=1e-2,
                 n_batch=self.bsz,
                 backprop=False,
                 verbose=0,
@@ -574,10 +576,11 @@ class Tracking_MPC(torch.nn.Module):
         if self.args.solver_type == "al":
             xu_ref = torch.cat([x_ref, u_ref], dim=-1)
             if self.x_init is None:
-                self.x_init = self.ctrl.x_init = x_ref
-                self.u_init = self.ctrl.u_init = u_ref
+                self.x_init = self.ctrl.x_init = x_ref.detach().clone()
+                self.u_init = self.ctrl.u_init = u_ref.detach().clone()
 
         self.compute_p(xu_ref)
+        # ipdb.set_trace()
         if self.args.solver_type == "al":
             cost = al_utils.QuadCost(self.Q, self.p)
         else:

deqmpc/train_simple.py
@@ -164,7 +164,7 @@ def main():
                 )
                 losses_var[j].append(torch.var(loss_j_proxy).item())
                 loss_j = loss_j.mean()
-                loss += loss_j
+                loss += loss_j + 0.1*loss_j_proxy.mean()
                 losses_iter[j].append(loss_j_proxy.mean().item())
                 
                 # gt_state = traj_sample["state"]

qpth/AL_mpc.py
@@ -247,8 +247,8 @@ class MPC(Module):
             cost = al_utils.QuadCost(cost.C.diagonal(dim1=-2, dim2=-1), cost.c)
         x, u = self.al_solve(x, u, dx, dx_jac, x0, cost)
         
-        self.x_init = x
-        self.u_init = u
+        self.x_init = x.detach().clone()
+        self.u_init = u.detach().clone()
         return (x, u)
     
     def al_solve(self, x, u, dx, dx_jac, x0, cost, lamda_init=None, rho_init=None):
@@ -265,7 +265,6 @@ class MPC(Module):
             rho = self.rho_prev
         else:
             rho = rho_init
-        # ipdb.set_trace()
         x_old, u_old = x, u
         with torch.no_grad():
             dyn_res_clamp_start = self.dyn_res(torch.cat((x, u), dim=2), dx, x0, res_type='clamp').view(self.n_batch, -1)
@@ -277,11 +276,14 @@ class MPC(Module):
                 rho_hist = torch.stack(self.cost_lam_hist[2][::-1], dim=0)
                 lamda, rho = al_utils.warm_start_al(x, lamda, rho, cost_start, cost_hist, lamda_hist, rho_hist)        
         rho_init = rho
-        Q = cost.C.to(self.dtype)
-        q = cost.c.to(self.dtype)
+        Q = cost.C#.to(self.dtype)
+        q = cost.c#.to(self.dtype)
         cost_lam_hist = [[cost_start], [lamda], [rho]]
         # end1 = time.time()
         # Augmented Lagrangian updates
+        # self.verbose = 1
+
+        # ipdb.set_trace()
         for i in range(self.al_iter):
             # start2 = time.time()
             xu = torch.cat((x, u), dim=2).detach().clone()
@@ -305,16 +307,16 @@ class MPC(Module):
                     print("iter :", i, dyn_res_clamp.mean().item(), cost_res.mean().item(), rho.mean().item())
                 # print("iter :", i, dyn_res_clamp.mean().item(), cost_res.mean().item(), rho.mean().item())
                 
-                rho = torch.minimum(rho*10*status[:,None] + rho*(1-status[:,None]), rho_init*100)
-                # rho = rho * 10
+                # rho = torch.minimum(rho*10*status[:,None] + rho*(1-status[:,None]), rho_init*100)
+                rho = rho * 10
                 cost_lam_hist[0].append(cost_res)
                 cost_lam_hist[1].append(lamda)
                 cost_lam_hist[2].append(rho)
             # end3 = time.time()
             # print("outer time: ", end1 - start1, end2 - start2, end3 - end2)
         
-        # if self.verbose > 0:
-        #     ipdb.set_trace()
+        if self.verbose > 0:
+            ipdb.set_trace()
         self.cost_lam_hist = cost_lam_hist
         self.lamda_prev = lamda
         self.rho_prev = rho

qpth/al_utils.py
@@ -95,7 +95,9 @@ def merit_grad_hessian(
     if diag_cost:
         Qfull = torch.diag_embed(Q.reshape(bsz, -1))
     merit_hess = Qfull + rho[:, :, None] * constraint_hess
-    return merit_grad, merit_hess
+    merit_hess_clip = Qfull + torch.clamp(rho[:, :, None], max=10) * constraint_hess
+    # print(constraint_hess.norm().item(), Qfull.norm().item(), constraint_jac_clamp.norm().item(), constraint_jac.norm().item())
+    return merit_grad, merit_hess, merit_hess_clip
 
 
 def merit_hessian(
@@ -189,13 +191,13 @@ def dyn_res_eq(
     x_next = dx(x[:, :-1].reshape(-1, xsize), u[:, :-1].reshape(-1, usize)).view(
         bsz, T - 1, xsize
     )
-    res = x[:, 1:, :] - x_next
+    res_d = x[:, 1:, :] - x_next
     res_init = (x[:, 0, :] - x0).reshape(bsz, 1, -1)
     # if self.add_goal_constraint:
     #     res_goal = (x[:,-1,:] - self.x_goal).reshape(bsz, -1)
     #     res = torch.cat((res, res_init, res_goal), dim=1)
     # else:
-    res = torch.cat((res, res_init), dim=1)
+    res = torch.cat((res_d, res_init), dim=1)
     res = res.reshape(bsz, -1)
     return res
 
@@ -285,12 +287,16 @@ def dyn_res_ineq(x, u, x0, x_lower, x_upper, u_lower, u_upper):
 
 def dyn_res_ineq_jac(x, u, x0, x_lower, x_upper, u_lower, u_upper):
     bsz, T, x_size = x.shape
-    res, res_clamp, id_xu = dyn_res_ineq_jac_jit(
+    res, res_clamp, id_xu, clamp_idx = dyn_res_ineq_jac_jit(
         x, u, x0, x_lower, x_upper, u_lower, u_upper
     )
     id_xu = torch.block_diag(*id_xu)
     id_xu = id_xu[None].repeat(bsz, 1, 1)
-    id_xu_clamp = id_xu * (res_clamp > 0).float()[..., None]
+    # ipdb.set_trace()
+    # id_xu_clamp = id_xu * (res_clamp > 0).float()[..., None]
+    id_xu_clamp = id_xu * clamp_idx.float()[..., None]
+    # print("ineqjac :", id_xu_clamp.norm().item(), id_xu.norm().item(), res_clamp.norm().item(), res.norm().item())
+    # ipdb.set_trace()
     return res, res_clamp, id_xu, id_xu_clamp
 
 
@@ -301,13 +307,14 @@ def dyn_res_ineq_jac_jit(x, u, x0, x_lower, x_upper, u_lower, u_upper):
     res = None
     res = torch.cat((u - u_upper, -u + u_lower), dim=2)
     res = res.reshape(bsz, -1)
+    clamp_idx = res >= 0
     res_clamp = torch.clamp(res, min=0)
     id_u = torch.eye(u_size, device=x.device).to(x)
     id_u = torch.cat((id_u, -id_u), dim=0)
     id_x = torch.zeros((2 * u_size, x_size), device=x.device).to(x)
     id_xu = torch.cat((id_x, id_u), dim=1)
     id_xu = [id_xu] * T
-    return res, res_clamp, id_xu
+    return res, res_clamp, id_xu, clamp_idx
 
 
 def dyn_res(xu, dx, x0, x_lower=None, x_upper=None, u_lower=None, u_upper=None):
@@ -392,19 +399,23 @@ class NewtonAL(torch.autograd.Function):
         if verbose:
             print(nstep, (dyn_res.view(bsz, -1).norm(dim=-1)).mean().item(), (cost).mean().item(), merit.mean().item())
         stepsz = 1
-        cholesky_fail = torch.tensor(False)
+        cholesky_fail = torch.tensor(True)
         merit_delta = 1
-        while (
-            merit_delta > threshold*1e-8 and nstep < max_newton_steps):# and stepsz > 1e-8
+        # ipdb.set_trace()
+        # while (
+        #     merit_delta > threshold*1e-8 and nstep < max_newton_steps):# and stepsz > 1e-8
+        while nstep < 4 and stepsz > 1e-8:
         # ):  # and update_norm > 1e-3*init_update_norm:
             # ipdb.set_trace()
             nstep += 1
             # Compute the hessian and gradient of the augmented lagrangian
             with torch.enable_grad():
                 x_est.requires_grad_(True)
-                grad, Hess = meritGHfnQ(x_est)
+                grad, Hess, Hess_clip = meritGHfnQ(x_est)
+            # ipdb.set_trace()
             # Solve for the newton step
             stepsz = 0
+            # Hess_clip = Hess = torch.eye(Hess.shape[-1], device=Hess.device, dtype=Hess.dtype).repeat(bsz, 1, 1) 
             if not cholesky_fail:
                 U, info = torch.linalg.cholesky_ex(Hess)
                 update = -torch.cholesky_solve(grad.reshape(bsz, -1, 1), U).reshape(
@@ -416,41 +427,48 @@ class NewtonAL(torch.autograd.Function):
                     )
                     cholesky_fail = torch.tensor(True)
             else:
+                U = Hess
+                # update = -grad.reshape(#torch.linalg.solve(Hess, grad.reshape(bsz, -1)).reshape(
+                #     bsz, T, n_elem
+                # )
                 update = -torch.linalg.solve(Hess, grad.reshape(bsz, -1)).reshape(
                     bsz, T, n_elem
                 )
+                # update = torch.round(update, decimals=2)
 
             if ls:
-                x_est, new_merit, stepsz, status = line_search_newton(
+                x_est1, new_merit, stepsz, status = line_search_newton(
                     update, x_est, meritfnQ, merit, x0
                 )
             else:
-                x_est = x_est + update
+                x_est1 = x_est + update
                 new_merit = meritfnQ(x_est)
 
             if (
-                x_est.isnan().sum() > 0 or x_est.isinf().sum() > 0
+                x_est1.isnan().sum() > 0 or x_est1.isinf().sum() > 0
             ):  # or new_merit.isnan().sum() > 0 or new_merit.isinf().sum() > 0:
                 ipdb.set_trace()
-            cost = cost_fnQ(x_est)
-            dyn_res = dyn_fn(x_est)
+            cost = cost_fnQ(x_est1)
+            dyn_res = dyn_fn(x_est1)
             new_dyn_res = torch.norm(dyn_res).item()
             if verbose:
                 print(nstep, (dyn_res.view(bsz, -1).norm(dim=-1)).mean().item(), (cost).mean().item(), torch.norm(update).item(), new_merit.mean().item(), stepsz)
-
+            # if nstep == 3:
+            #     ipdb.set_trace()
+            x_est = x_est1
             ## exit creteria
-            # if (
-            #     abs(old_dyn_res - new_dyn_res) / new_dyn_res < 1e-3
-            #     or new_dyn_res < 1e-3
-            # ):
-            #     break
+            if (
+                abs(old_dyn_res - new_dyn_res) / new_dyn_res < 1e-3
+                or new_dyn_res < 1e-3
+            ):
+                break
 
             old_dyn_res = new_dyn_res
             merit_delta = 1000# ((new_merit - merit) / new_merit).abs().max().item()
             merit = new_merit
 
         try:
-            ctx.save_for_backward(Hess, U, x_est, cholesky_fail)
+            ctx.save_for_backward(Hess_clip, U, x_est, cholesky_fail)
         except:
             ipdb.set_trace()
         Us, VTs = None, None
@@ -507,7 +525,7 @@ def line_search_newton(update, x_est, meritfnQ, merit, x0):
         .expand(n_ls, x_est.shape[0])
     )
     x_next = x_est[None] + stepszs[:, :, None, None] * update[None]
-    x_next[:, :, 0, :xsize] = x0[None]
+    # x_next[:, :, 0, :xsize] = x0[None]
     # new2_objective = torch.stack([meritfnQ(x_next[i]) for i in range(n_ls)], dim=0)
     # new2_objective = torch.vmap(meritfnQ)(x_next)
     new2_objective = meritfnQ(x_next).reshape(n_ls, -1)